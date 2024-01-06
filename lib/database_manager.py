8bf98de96f9ba3fa8245dd9078b4c2c182a9e27c
Jald <Artem.Ryzhenkov@yandex.ru>
2024-01-06 20:55:34 +0300

    Add database_manager

---
+++ lib/database_manager.py
@@ -0,0 +1,105 @@
+from abc import ABC
+from dataclasses import dataclass
+from sqlite3 import connect, Connection, Cursor
+from typing import Optional
+
+DB_NAME = 'SteamTradeBot.db'
+
+
+@dataclass
+class DatabaseManagerBase(ABC):
+    """Base class for interacting with a database."""
+
+    db_name: str = DB_NAME
+    conn: Optional[Connection] = None
+    cursor: Optional[Cursor] = None
+
+    def connect(self) -> None:
+        """Create connection."""
+        self.conn: Connection = connect(self.db_name)
+        if not self.conn:
+            raise Exception('No connection')
+
+        self.cursor: Cursor = self.conn.cursor()
+
+    def close_connect(self) -> None:
+        """Close connection."""
+        if self.conn:
+            self.conn.close()
+
+
+class DatabaseManager(DatabaseManagerBase):
+    """Class for interacting with a database."""
+
+    def create_table(self, table_name: str, fields: list[str]) -> None:
+        """Table creation method.
+
+        Args:
+            table_name: table name.
+            fields: fields to create (e.g. firstname TEXT or age INTEGER).
+        """
+        self.connect()
+        self.cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {table_name} (
+            id INTEGER PRIMARY KEY,
+            {', '.join(fields)}
+            )
+            ''')
+        self.conn.commit()
+        self.close_connect()
+
+    def check_table_exist(self, table_name: str) -> bool:
+        """Method for checking whether a table has been created.
+
+        Args:
+            table_name: table name.
+
+        Returns:
+            True - table exists, False - table not exists.
+        """
+        self.connect()
+        self.cursor.execute(f'''
+            SELECT name
+            FROM sqlite_master
+            WHERE type='table' AND name='{table_name}'
+        ''')
+        result = self.cursor.fetchone()
+        self.close_connect()
+        return result is not None
+
+    def insert_table_data(self, table_name: str, data: dict) -> None:
+        """Method for adding new data to a table.
+
+        Args:
+            table_name: table name.
+            data: data dict.
+        """
+        if not self.check_table_data_exist(table_name, data):
+            self.connect()
+            columns = ', '.join(data.keys())
+            values = ', '.join([f":{key}" for key in data.keys()])
+            self.cursor.execute(f'''
+                INSERT INTO {table_name} ({columns})
+                VALUES ({values})''', data)
+            self.conn.commit()
+            self.close_connect()
+
+    def check_table_data_exist(self, table_name: str, data: dict) -> bool:
+        """Method for checking the existence of data in a table.
+
+        Args:
+            table_name: table name.
+            data: data dict.
+
+        Returns:
+            True - data in table, False - data not in table.
+        """
+        self.connect()
+        clause = " AND ".join([f"{key} = :{key}" for key in data.keys()])
+        self.cursor.execute(f'''
+            SELECT *
+            FROM {table_name}
+            WHERE {clause}''', data)
+        result = self.cursor.fetchone()
+        self.close_connect()
+        return result is not None
---
+++ tests/lib/database_manager_test.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from lib.database_manager import DatabaseManager
+
+
+class TestDatabaseManager(TestCase):
+
+    def setUp(self) -> None:
+        self.instance = DatabaseManager('TestDB.db')
+
+    def test_create_table(self):
+        self.instance.create_table('test', ['firstname TEXT', 'lastname TEXT', 'age INTEGER'])
+        with self.subTest('Table exist'):
+            self.assertTrue(self.instance.check_table_exist('test'))
+        with self.subTest('Table not exist'):
+            self.assertFalse(self.instance.check_table_exist('test_'))
+
+    def test_insert_table_data(self):
+        self.instance.create_table('test', ['firstname TEXT', 'lastname TEXT', 'age INTEGER'])
+
+        insert_data = {'firstname': 'Bob', 'lastname': 'Orange', 'age': 101}
+        self.instance.insert_table_data('test', insert_data)
+        with self.subTest('Data is already in the table'):
+            self.assertTrue(self.instance.check_table_data_exist('test', insert_data))
+        with self.subTest('No data in the table'):
+            insert_data['firstname'] = 'Alex'
+            self.assertFalse(self.instance.check_table_data_exist('test', insert_data))
