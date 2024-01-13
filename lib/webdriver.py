59ee87131d177058d38cdd9f7f279745bba1858b
Jald <Artem.Ryzhenkov@yandex.ru>
2024-01-13 22:54:02 +0300

    Add doc and typing for webdriver

--- lib/webdriver.py
+++ lib/webdriver.py
@@ -11,6 +11,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 @dataclass
 class DriverSettingsBase(ABC):
+    """Base class for interacting with driver options."""
     settings: Options = Options()
 
     @property
@@ -21,21 +22,29 @@ class DriverSettingsBase(ABC):
 
 @dataclass
 class DriverSettings(DriverSettingsBase):
+    """Class for interacting with driver options."""
     custom_settings: Optional[tuple] = None
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
+        """Post initialization."""
         settings = webdriver_settings
         if custom_settings := self.custom_settings:
             settings += custom_settings
         _ = [self.settings.add_argument(i) for i in settings]
 
     @property
-    def get_settings(self):
+    def get_settings(self) -> Options:
+        """Method for getting an instance of the browser settings class.
+
+        Returns:
+            An instance of the browser settings class with the settings applied.
+        """
         return self.settings
 
 
 @dataclass
 class DriverBase(ABC):
+    """Base class for getting an instance of the driver class."""
     settings: DriverSettings = DriverSettings()
     driver_type: str = 'chrome'
     driver: Optional[WebDriver] = None
@@ -43,9 +52,11 @@ class DriverBase(ABC):
 
 @dataclass
 class Driver(DriverBase):
+    """Class for getting an instance of the driver class."""
     custom_settings: Optional[tuple] = None
 
     def __post_init__(self) -> None:
+        """Post initialization."""
         if self.driver_type.lower() == 'chrome':
             self.driver = Chrome(
                 options=DriverSettings(
@@ -56,13 +67,32 @@ class Driver(DriverBase):
         raise Exception('Driver not found')
 
     @property
-    def get_driver(self):
+    def get_driver(self) -> Chrome:
+        """Method for getting an instance of a Chrome class with settings applied.
+
+        Returns:
+            An instance of a Chrome class with settings applied.
+        """
         return self.driver
 
 
-def get_user_agent(driver: Union[Driver, WebDriver]):
+def get_user_agent(driver: Union[Driver, WebDriver]) -> str:
+    """Method for getting user-agent.
+
+    Args:
+        driver: driver instance.
+
+    Returns:
+        User-agent.
+    """
     return driver.execute_script("return navigator.userAgent;")
 
 
-def add_cookies(driver: Union[Driver, WebDriver], cookies: list[dict]):
+def add_cookies(driver: Union[Driver, WebDriver], cookies: list[dict]) -> None:
+    """Method for adding cookies.
+
+    Args:
+        driver: driver instance.
+        cookies: list with cookies.
+    """
     _ = [driver.add_cookie(i) for i in cookies]
